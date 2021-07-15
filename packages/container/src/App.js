0d80cf19d88840e07683cce5abbbd2996c04c480
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-15 15:59:22 +0200

    Fixing className prefix

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,16 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
+const generateClassName =  createGenerateClassName({
+    productionPrefix: 'ctn',
+});
 
 export default () => {
    return(
-      <BrowserRouter>
-         <div>
-            <Header />
-            <MarketingApp />
-         </div>
-      </BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+         <BrowserRouter>
+            <div>
+               <Header />
+               <MarketingApp />
+            </div>
+         </BrowserRouter>
+      </StylesProvider>
    );
    
 };
