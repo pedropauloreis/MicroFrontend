a358bed59a11d4c43a6e9cf3331c92d42f83b6c8
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-15 13:48:38 +0200

    creating stg WFs

---
+++ packages/container/config/webpack.stg.js
@@ -0,0 +1,27 @@
+const {merge} = require('webpack-merge');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const commonConfig = require('./webpack.common');
+const packageJson = require('../package.json');
+
+const domain = process.env.STAGE_DOMAIN;
+
+const prodConfig = {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest/'
+    },
+    plugins: [
+        new ModuleFederationPlugin({
+          name: 'container',
+          remotes: {
+            marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+          },
+          shared: packageJson.dependencies,
+        }),
+  
+    ],
+  };
+
+  module.exports = merge(commonConfig, prodConfig);
+  
