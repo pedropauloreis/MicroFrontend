407a65fa2d62deac6faed3f678b2e8bf30139ac5
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-15 18:35:26 +0200

    fixing navigation sync

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 
-const mount = (el) => {
+const mount = (el, {onNavigate, defaultHistory}) => {
+    const history = defaultHistory || createMemoryHistory();
+    
+    if(onNavigate)
+    {
+        history.listen(onNavigate);
+    }
+
+
+    
     ReactDOM.render(
-        <App />,
+        <App history={history} />,
         el
     );
+
+    return {
+        onParentNavigate({pathname: nextPathname}) {
+            const {pathname} = history.location;
+            if(pathname !== nextPathname)
+            {
+                history.push(nextPathname);
+            }
+            
+        }
+    }
 };
 
 //Context #1
@@ -23,7 +44,7 @@ if (process.env.NODE_ENV === 'development')
     if (devRoot)
     {
         //We are running in isolation
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
