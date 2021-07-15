6aa18a30925ada04b09fb0e6b1e17588d6163282
Pedro Paulo de Oliveira Reis <ppreis@gmail.com>
2021-07-15 15:24:21 +0200

    Adding header in container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp'
+import Header from './components/Header'
 
 
 export default () => {
-   return <>
-      <h1>Hi there.</h1>
-      <hr />
-      <MarketingApp />
-   </>
+   return(
+      <BrowserRouter>
+         <div>
+            <Header />
+            <MarketingApp />
+         </div>
+      </BrowserRouter>
+   );
+   
 };
